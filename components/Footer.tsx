2ef8e405cec0379f99fdf489d030140bd4f9c253
Vincent <vincent@bytesbase.com>
2022-07-03 16:35:01 -0500

    styled footer + added font awesome

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebookSquare,
+} from "@fortawesome/free-brands-svg-icons";
+export default function Footer(): React.ReactElement {
+  const router = useRouter();
+  const isHomePage = router.asPath === "/";
+  const currentYear = new Date().getFullYear();
 
-export default function Footer() {
-  return <div>Footer</div>;
+  return !isHomePage ? (
+    <footer className="bg-[#D3DFDF] w-full flex items-center justify-center flex-col py-16 text-white">
+      <div className="flex">
+        <a href="https://instagram.com" target="_blank">
+          <FontAwesomeIcon className="mr-4 w-4" icon={faInstagram} />
+        </a>
+        <a href="https://facebook.com" target="_blank">
+          <FontAwesomeIcon className="w-4" icon={faFacebookSquare} />
+        </a>
+      </div>
+
+      <p className="mt-6">copyright {currentYear} good soup studio</p>
+    </footer>
+  ) : (
+    <></>
+  );
 }
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,14 +5,14 @@ import Head from "next/head";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/vte3dhv.css" />
       </Head>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
